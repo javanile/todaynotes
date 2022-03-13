442315c08f508e80779bc483900ffc2ca68f45e3
Francesco Bianco <info.francescobianco@gmail.com>
2022-03-13 17:41:52 +0100

    First commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
 
     <div class="py-12">
         <div class="mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden sm:rounded-lg">
 
                 @livewire('tools.notes')
 
--- resources/views/layouts/full-screen.blade.php
+++ resources/views/layouts/full-screen.blade.php
@@ -21,22 +21,10 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="h-screen bg-gray-100">
             @livewire('navigation-menu')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
             <!-- Page Content -->
-            <div>
-                Full screen
-            </div>
             <main>
                 {{ $slot }}
             </main>
